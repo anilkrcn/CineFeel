5c73905a560e938df7ae3cc009e5a4f80e593742
Anıl Karacan <aanilkaracann@gmail.com>
2026-05-07 21:41:10 +0300

    Initial Commit

---
+++ CineFeel/CineFeelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineFeelApp.swift
+//  CineFeel
+//
+//  Created by Anıl Karacan on 7.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct CineFeelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
